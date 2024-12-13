f30aa3e827552d5dc86adea2133552a7a72d7f5f
Denis Ryzhankov <denryzhankov@gmail.com>
2024-12-13 22:58:15 +0300

    Добавить Партии и тесты

---
+++ model.py
@@ -0,0 +1,38 @@
+from typing import Optional, Set
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str  # единица складского учета (stock-keeping unit)
+    qty: int  # количество (quantity)
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta  # предполагаемый срок прибытия (estimated arrival time, ETA)
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
