3e9d2f47e95dde705f347db8a3074f14272f3d74
Denis Ryzhankov <denryzhankov@gmail.com>
2025-01-07 05:25:08 +0300

    add test_reposetory.py and repository.py

---
+++ repository.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+import abc
+import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        print(select(model.Batch).filter_by())
+        print(select(model.Batch).where(model.Batch.reference == reference))
+        # return self.session.query(model.Batch).filter_by(reference).one()
+        return self.session.scalars(
+            select(model.Batch).where(model.Batch.reference == reference)
+        ).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
